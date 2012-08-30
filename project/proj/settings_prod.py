20b80f0188cbc1c91d8312c4fb47369536f032ae
Alexandrov-Michael <mikle.alex@gmail.com>
2012-08-30 15:49:50 +0400

    setting prod

--- project/proj/settings_prod.py
+++ project/proj/settings_prod.py
@@ -5,11 +5,11 @@ __author__ = 'michael'
 
 DATABASES = {
     'default': {
-        'ENGINE': '', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'wproduktru_zont',                      # Or path to database file if using sqlite3.
+        'USER': 'wproduktru_zont',                      # Not used with sqlite3.
+        'PASSWORD': 'jjrroo5004kioogg',                  # Not used with sqlite3.
+        'HOST': 'pg.sweb.ru',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                     # Set to empty string for default. Not used with sqlite3.
     }
 }
